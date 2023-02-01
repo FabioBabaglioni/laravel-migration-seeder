238ce08112b9f59f3d95f48e9178352666a13c2d
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-02-01 16:18:28 +0100

    Creo colonne tabella train e Travel

--- database/migrations/2023_02_01_144457_create_train.php
+++ database/migrations/2023_02_01_144457_create_train.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('train', function (Blueprint $table) {
             $table->id();
 
-            $table -> char('code', 10) -> unique(true);
+            $table -> char('code', 10) -> unique();
             $table -> char('departureStation', 40);
-            $table -> char('departureStation', 40);
+            $table -> char('arrivalStation', 40);
             $table -> dateTime('departureTime');
             $table -> dateTime('arrivalTime');
             $table -> smallInteger('numberOfCarriage');
--- database/migrations/2023_02_01_144512_create_travel.php
+++ database/migrations/2023_02_01_144512_create_travel.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -15,6 +16,11 @@ return new class extends Migration
     {
         Schema::create('travel', function (Blueprint $table) {
             $table->id();
+
+            $table->char('description', 255);
+            $table->char('destination', 40);
+            $table->char('tripCode', 10) -> Unique();
+
             $table->timestamps();
         });
     }
